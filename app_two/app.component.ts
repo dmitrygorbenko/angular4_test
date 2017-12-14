a4ea2b01376c13993c481b62a7f22b7cb710d95c
Dima Gorbenko <dmitriy.gorbenko@gmail.com>
2017-12-14 20:52:16 +0200

    initial import

---
+++ app_two/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'app_two',
+	templateUrl: 'app_one/app.component.html',
+	styleUrls: ['app_one/app.component.css']
+})
+export class AppComponent {
+	title = 'App 2';
+}
