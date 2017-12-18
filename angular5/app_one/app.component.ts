eea73ccce14bdfc5affe8e6d2e3ededbfb9e9032
Dima Gorbenko <dmitriy.gorbenko@gmail.com>
2017-12-18 18:43:34 +0200

    addin changes

---
+++ angular5/app_one/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'ang5_app_one',
+	templateUrl: 'angular5/app_one/template.html',
+	styleUrls: ['angular5/app_one/styles.css']
+})
+export class AppComponent {
+	title = 'This is Angular 5 app #1';
+}
---
+++ angular5/app_two/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+	selector: 'ang5_app_two',
+	templateUrl: 'angular5/app_two/template.html',
+	styleUrls: ['angular5/app_two/styles.css']
+})
+export class AppComponent {
+	title = 'This is Angular 5 app #2';
+}
